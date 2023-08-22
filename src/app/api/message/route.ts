ace5317781cab2d86749e9b284668ab28ffbf696
R-Saunders <richsaunders@protonmail.com>
2023-08-22 09:50:38 +0100

    second attempt at vercel fix

--- src/app/api/message/route.ts
+++ src/app/api/message/route.ts
@@ -4,14 +4,15 @@ import { kv } from "@vercel/kv";
 import { NextRequest } from "next/server";
 
 export async function GET() {
-  // <string> between the get method and the actual invocation, I am telling the function what it will return
-  const message = await kv.get<string>("message");
-  return new Response(JSON.stringify(message));
+	// <string> between the get method and the actual invocation, tells the function what it will return
+	// const message = await kv.get<string>("message");
+	const message = "Hello Worlds";
+	return new Response(JSON.stringify(message));
 }
 
 export async function POST(request: NextRequest) {
-  const form = await request.formData();
-  const newmessage = form.get("messagefield") as string;
-  await kv.set("message", newmessage);
-  return new Response(JSON.stringify("success"));
+	const form = await request.formData();
+	const newmessage = form.get("messagefield") as string;
+	await kv.set("message", newmessage);
+	return new Response(JSON.stringify("success"));
 }
